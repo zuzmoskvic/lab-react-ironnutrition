225002315683f22609b2e138ad3954b4824e1d3d
“Zuzana <zuzana.moskvic@gmail.com>
2025-07-24 20:55:30 +0200

    Iteration 3

--- src/App.js
+++ src/App.js
@@ -5,27 +5,10 @@ import FoodBox from './components/FoodBox';
 
 function App () {
   const [foods, setFoods] = useState(foodsData);
-  const [name, setName] = useState("");
-  const [food, setFood] = useState({});
 
   return <div className="App">
 
-    <FoodBox food={ {
-        name: "Orange",
-        calories: 85,
-        image: "https://i.imgur.com/abKGOcv.jpg",
-        servings: 1
-    }} />
-
-    { foods.map((food)=>{
-        return (
-          <div>
-            <p> {food.name} </p>
-            <img src={food.image} width={100} alt={food.name}/>
-          </div>
-        )
-    }
-    )}
+    <FoodBox foods={foods}/>
 
   </div>;
 }
--- src/components/FoodBox.js
+++ src/components/FoodBox.js
@@ -1,14 +1,18 @@
-function FoodBox({food}) {
-  
-  return <div>
-    <div><h1>{food.name}</h1></div>
-    <div>
-        <img src={food.image} width={100} alt={food.name}/>
-        <p>Calories: {food.calories}</p>
-        <p>Servings: {food.servings}</p>
-        <p>Total Calories: {food.calories * food.servings} kcal</p>
-        <button>Delete</button>
-    </div>
-  </div>
+function FoodBox({foods}) {
+  return <>
+    { foods.map((food) => {
+        return (
+        <div>
+            <div><h1>{food.name}</h1></div>
+            <div>
+                <img src={food.image} width={100} alt={food.name}/>
+                <p>Calories: {food.calories}</p>
+                <p>Servings: {food.servings}</p>
+                <p>Total Calories: {food.calories * food.servings} kcal</p>
+                <button>Delete</button>
+            </div>
+        </div>)
+    })}
+  </>
 }
 export default FoodBox;
