ffe17aeb1639484dd6fd1b0a8d1960b4f9ce5161
“Zuzana <zuzana.moskvic@gmail.com>
2025-07-24 13:34:47 +0200

    Iteration 2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-// src/App.js
 import { useState } from 'react';
 import './App.css';
 import foodsData from "./foods.json";
+import FoodBox from './components/FoodBox';
 
 function App () {
   const [foods, setFoods] = useState(foodsData);
+  const [name, setName] = useState("");
+  const [food, setFood] = useState({});
+
   return <div className="App">
+
+    <FoodBox food={ {
+        name: "Orange",
+        calories: 85,
+        image: "https://i.imgur.com/abKGOcv.jpg",
+        servings: 1
+    }} />
+
     { foods.map((food)=>{
         return (
           <div>
             <p> {food.name} </p>
-            <img src={food.image} width={100} />
+            <img src={food.image} width={100} alt={food.name}/>
           </div>
         )
     }
